fb4fd791411f13db911b7beaeebb19c10bd9687f
waadahmedtourad <waadahmedshabaan@gmail.com>
2024-07-27 12:58:34 +0300

    Task_js3

---
+++ js/main.js
@@ -0,0 +1,76 @@
+//Task1
+/* function checkEvenOrOdd() {
+    let num = prompt("Enter Your Number");
+    num = Number(num); 
+
+    if (num % 2 === 0) {
+        console.log("The Number is Even");
+    } else {
+        console.log("The Number is Odd");
+    }
+}
+checkEvenOrOdd();
+ */
+//Task2
+/* const calculateArea = function(radius) {
+    const pi = Math.PI;
+    const area = pi * Math.pow(Number(radius), 2); 
+    return "The Area is = " + area;
+};
+
+const calculateCircumference = function(radius) {
+    const pi = Math.PI;
+    const circumference = 2 * pi * Number(radius); 
+    return "The Circumference is = " + circumference;
+};
+
+const radius = prompt("Enter Your radius");
+const area = calculateArea(radius);
+const circumference = calculateCircumference(radius);
+
+console.log(area);
+console.log(circumference); */
+//Task3
+/* const Sum = (num1, num2) => {
+    num1 = Number(num1);
+    num2 = Number(num2);
+    
+    return num1 + num2;
+};
+
+let num1 = prompt("Enter Your firstNumber");
+let num2 = prompt("Enter Your secondNumber");
+
+const sum = Sum(num1, num2);
+console.log("The sum is = " + sum); */
+//Task4
+/*function checkNumber(num) {
+    if (num > 0) {
+        console.log("Positive");
+    } else if (num < 0) {
+        console.log("Negative");
+    } else {
+        console.log(0);
+    }
+}
+
+let num = prompt("Enter Your Number");
+num = Number(num);
+
+checkNumber(num);*/
+//Task5
+/* const reverseString = (str) => {
+    return str.split('').reverse().join('');
+};
+
+let words = prompt("Enter Your Words");
+let array = [words]; 
+
+array[0] = reverseString(array[0]);
+console.log(array[0]); */
+
+
+
+
+
+
